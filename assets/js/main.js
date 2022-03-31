e9d05fafe97dc0224f4f4e715d377737ebc4c708
Ilix-studio <ilishjyoti17@gmail.com>
2022-03-31 05:32:18 +0530

    added music section in footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,4 +58,41 @@ sr.reveal('.contact__input',{interval: 200});
 
 
 
+const musicContainer = document.getElementById('music-container');
+const cover = document.getElementById('cover');
+const playBtn = document.getElementById('play');
+const audio = document.getElementById('audio');
+const title = document.getElementById('title');
 
+// Song titles
+const songs = ['misunderstood'];
+
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+  
+    audio.play();
+  }
+
+  // Pause song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  
+    audio.pause();
+  }
+
+  // Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+  
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  });
